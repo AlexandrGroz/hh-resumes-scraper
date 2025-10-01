c1b423e2ff43b98104d94c4db47b6b3dc367cdd9
Alexandr Groz <79365094+AlexandrGroz@users.noreply.github.com>
2025-10-02 04:19:47 +0500

    Streamline resume scraping pipeline

--- actions/login/login.py
+++
@@ -1,9 +0,0 @@
-from actions.login.check_auth import check_auth
-from actions.login.choose_login import choose_login
-
-
-def login():
-    if check_auth():
-        return
-    else:
-        choose_login()
